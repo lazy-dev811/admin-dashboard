cc441aa7a7182fdc8872ebc6f03514d06df490ab
devsun527 <dev.sun0811@gmail.com>
2016-10-16 17:52:59 +0200

    removed old files

--- src/client/app/reducers/index.js
+++ src/client/app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import books from './books';
 import widgetGreet from '../components/Greet/redux/reducer';
 import widgetWeather from '../components/Weather/redux/reducer';
 
 const rootReducer = combineReducers({
-  books,
   widgetGreet,
   widgetWeather,
 });
