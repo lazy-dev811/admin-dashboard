14bbcd2b15b4c26f7e3dd9f096940746eab8f74f
devsun527 <dev.sun0811@gmail.com>
2016-11-26 22:11:03 +0200

    feature(icon): added chevronRight and chevronLeft

---
+++ src/client/app/components/Icon/ChevronLeft.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ChevronLeft = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="svg"
+    x="0px"
+    y="0px"
+    viewBox="0 0 357 357"
+  >
+    <g id="chevron-left">
+      <polygon points="298.052,24 266.052,0 112.206,205.129 266.052,410.258 298.052,386.258 162.206,205.129 " />
+    </g>
+  </svg>
+);
+
+export default ChevronLeft;
---
+++ src/client/app/components/Icon/ChevronRight.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ChevronRight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="svg"
+    x="0px"
+    y="0px"
+    viewBox="0 0 357 357"
+  >
+    <g id="chevron-right">
+      <polygon points="144.206,0 112.206,24 248.052,205.129 112.206,386.258 144.206,410.258 298.052,205.129 "/>
+    </g>
+  </svg>
+);
+
+export default ChevronRight;
