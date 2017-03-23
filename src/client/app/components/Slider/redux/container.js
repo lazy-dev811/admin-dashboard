80bf98b2c4ded93982ddb3668ffcce3697e39c93
devsun527 <dev.sun0811@gmail.com>
2017-03-23 21:43:23 +0200

    Slider: no longer a widget

--- src/client/app/components/Slider/redux/container.js
+++ src/client/app/components/Slider/redux/container.js
@@ -1,16 +1,14 @@
 import { connect } from 'react-redux';
 
 import { slideItemSelect } from './actions';
-import widgetHOC from '../../Widget/Widget.jsx';
 import Slider from '../Slider.jsx';
 
 const mapStateToProps = ({ widgetSlider, config }) => ({
   ...widgetSlider,
-  ...config,
   config,
 });
 
-const mergeProps = ({ sliderItems, activeWidgets, widgetSlider, config }, { dispatch }) => {
+const mergeProps = ({ sliderItems, activeWidgets, config }, { dispatch }) => {
   const widgetNames = Object
     .keys(config)
     .filter(widget => config[widget].type === 'widget')
@@ -20,8 +18,7 @@ const mergeProps = ({ sliderItems, activeWidgets, widgetSlider, config }, { disp
     sliderItems: widgetNames,
     activeWidgets,
     slideItemSelect: widgetName => dispatch(slideItemSelect(widgetName)),
-    config: widgetSlider,
   });
 };
 
-export default connect(mapStateToProps, null, mergeProps)(widgetHOC(Slider));
+export default connect(mapStateToProps, null, mergeProps)(Slider);
