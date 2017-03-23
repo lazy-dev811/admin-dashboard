c3bcc3c6f7bc2c6db10c07dd5545e40d7383bb70
devsun527 <dev.sun0811@gmail.com>
2017-03-23 21:42:43 +0200

    color picker no longer a widget

--- src/client/app/components/ColorPicker/redux/Container.js
+++ src/client/app/components/ColorPicker/redux/Container.js
@@ -2,17 +2,15 @@ import { connect } from 'react-redux';
 import { colorPickerSelectColor } from './actions';
 
 import ColorPicker from '../ColorPicker.jsx';
-import widgetHOC from '../../Widget/Widget.jsx';
 
 const mapStateToProps = ({ widgetColorPicker, config }) => ({ ...widgetColorPicker, ...config });
 
-const mergeProps = ({ widgetIdentifier, colors, activeColorName, widgetColorPicker }, { dispatch }) => ({
+const mergeProps = ({ widgetIdentifier, colors, activeColorName }, { dispatch }) => ({
   colors,
   activeColorName,
-  config: widgetColorPicker,
   selectColor(selectedColorName) {
     dispatch(colorPickerSelectColor(selectedColorName));
   },
 });
 
-export default connect(mapStateToProps, null, mergeProps)(widgetHOC(ColorPicker));
+export default connect(mapStateToProps, null, mergeProps)(ColorPicker);
