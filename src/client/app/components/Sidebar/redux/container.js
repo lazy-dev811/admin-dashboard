317be7599694cefacd4d34abbfb01ed4245dbad8
devsun527 <dev.sun0811@gmail.com>
2017-03-23 21:43:33 +0200

    Sidebar: no longer a widget

--- src/client/app/components/Sidebar/redux/container.js
+++ src/client/app/components/Sidebar/redux/container.js
@@ -1,14 +1,12 @@
 import { connect } from 'react-redux';
 
-import widgetHOC from '../../Widget/Widget.jsx';
 import Sidebar from '../Sidebar.jsx';
 
-const mapStateToProps = ({ widgetSidebar, config }) => ({ ...widgetSidebar, ...config });
+const mapStateToProps = ({ widgetSidebar }) => ({ ...widgetSidebar });
 
-const mergeProps = ({ loggedIn, sidebarItems, widgetSidebar }) => ({
+const mergeProps = ({ loggedIn, sidebarItems }) => ({
   loggedIn,
   sidebarItems,
-  config: widgetSidebar,
 });
 
-export default connect(mapStateToProps, null, mergeProps)(widgetHOC(Sidebar));
+export default connect(mapStateToProps, null, mergeProps)(Sidebar);
