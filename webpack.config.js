9cac7707d9680d12f7c0009e6db3c6ca5cd6bc54
devsun527 <dev.sun0811@gmail.com>
2017-03-28 20:46:12 +0200

    Upgradede webpack and refactored webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,130 @@
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-const APP_DIR = path.resolve(__dirname, 'src/client/app');
-const SCSS_DIR = path.resolve(__dirname, 'src/client/app');
-const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+// const APP_DIR = path.resolve(__dirname, 'src/client/app');
+const entryPath = './src/client/app/index.jsx';
+// const SCSS_DIR = path.resolve(__dirname, 'src/client/app');
+const outputPath = './src/client/public';
+// const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 
 const config = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    `${APP_DIR}/index.jsx`,
-  ],
+  entry: {
+    // 'webpack-dev-server/client?http://localhost:3000',
+    main: path.resolve(__dirname, entryPath),
+  },
 
   output: {
-    path: BUILD_DIR,
+    path: path.resolve(__dirname, outputPath),
     filename: 'bundle.js',
   },
 
   devServer: {
-    contentBase: './src/client',
+    inline: true,
+    port: 7777,
+    historyApiFallback: true,
   },
 
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devtool: 'source-map',
+
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.(js|jsx)$/,
-        include: APP_DIR,
-        loaders: ['babel-loader'],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /(node_modules|bower_components)/,
       },
       {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass'],
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   loaders: [
+      //     'file?name=nature-1.jpg',
+      //     'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+      //   ],
+      // },
+      // {
+      //   test: /\.(png|jpe?g)$/i,
+      //   loaders: [
+      //     'file-loader',
+      //     {
+      //       loader: 'image-webpack-loader',
+      //       query: {
+      //         progressive: true,
+      //         optimizationLevel: 7,
+      //         interlaced: false,
+      //         pngquant: {
+      //           quality: '65-90',
+      //           speed: 4,
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'highlight-loader',
+          },
+          {
+            loader: 'markdown-loader',
+          },
+        ],
       },
       {
-        test: /\.css$/,
-        loader: 'style!css?modules',
-        include: /flexboxgrid/,
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?name=nature-1.jpg',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'src/client/app/assets/fonts/[path][name].[ext]',
+            },
+          },
         ],
       },
     ],
   },
 
-  sassLoader: {
-    includePaths: SCSS_DIR,
-  },
+  // sassLoader: {
+  //   includePaths: SCSS_DIR,
+  // },
 
   plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/index.html',
+    }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
   ],
 };
 
