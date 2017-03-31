48d3f6c0f730650f63cf00916f0c65f63505ed66
devsun527 <dev.sun0811@gmail.com>
2017-04-01 01:34:10 +0200

    extended error messages

--- src/client/app/components/ErrorMessage/ErrorMessage.jsx
+++ src/client/app/components/ErrorMessage/ErrorMessage.jsx
@@ -3,15 +3,21 @@ import Error from '../Icon/Error.jsx';
 
 import './ErrorMessage.scss';
 
-const ErrorMessage = ({ message = 'An error occured' }) => (
-  <div className="error-message">
-    <Error className="error-message__icon" />
-    {message}
-  </div>
-);
+const ErrorMessage = ({ className = '', message = 'An error occured', position }) => {
+  const style = { position };
+
+  return (
+    <div className={`error-message ${className}`} style={style}>
+      <Error className="error-message__icon" />
+      {message}
+    </div>
+  );
+};
 
 ErrorMessage.propTypes = {
+  className: PropTypes.string,
   message: PropTypes.string,
+  position: PropTypes.string,
 };
 
 export default ErrorMessage;
