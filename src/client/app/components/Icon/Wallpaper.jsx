bf9111248344bf281224a5f1f519628481461b59
devsun527 <dev.sun0811@gmail.com>
2017-04-01 15:32:43 +0200

    added class to wallpaper icon component

--- src/client/app/components/Icon/Wallpaper.jsx
+++ src/client/app/components/Icon/Wallpaper.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Wallpaper = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    className="svg"
+    className="svg svg-wallpaper"
     viewBox="0 0 26 26"
   >
     <g>
