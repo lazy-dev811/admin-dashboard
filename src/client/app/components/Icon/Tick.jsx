27940fed6f238ab930525798dac36093f986bcb7
devsun527 <dev.sun0811@gmail.com>
2017-04-01 15:31:56 +0200

    added tick icon component

---
+++ src/client/app/components/Icon/Tick.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/* eslint max-len: 0 */
+const Tick = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="svg svg-tick"
+    viewBox="0 0 449 395"
+  >
+    <g>
+      <polygon points="142.8,323.85 35.7,216.75 0,252.45 142.8,395.25 448.8,89.25 413.1,53.55" />
+    </g>
+  </svg>
+);
+
+export default Tick;
