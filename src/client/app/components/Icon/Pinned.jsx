19e108ea5f9d1994fce89506aac3b026a784abb8
devsun527 <dev.sun0811@gmail.com>
2017-04-02 22:44:45 +0200

    added pin and pinned icon components

---
+++ src/client/app/components/Icon/Pinned.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/* eslint max-len: 0 */
+const Pinned = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="svg svg-pinned"
+    x="0px"
+    y="0px"
+    viewBox="0 0 224 512"
+  >
+    <g>
+      <path d="M224,112 c0,-61.844 -50.156,-112 -112,-112 s-112,50.156 -112,112 c0,56.375 41.797,102.563 96,110.375 V512 h32 V222.375 C182.188,214.563 224,168.375 224,112 z" />
+    </g>
+  </svg>
+);
+
+export default Pinned;
