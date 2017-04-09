bf71eff6336962335ab8eb069f4fdccf1c11e72a
devsun527 <dev.sun0811@gmail.com>
2017-04-09 15:32:46 +0200

    added chevronDown icon component

---
+++ src/client/app/components/Icon/ChevronDown.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/* eslint max-len: 0 */
+const ChevronDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="svg svg-chevron-down"
+    x="0px"
+    y="0px"
+    viewBox="0 0 410 186"
+  >
+    <g>
+      <polygon points="386.2569885253906,0 205.1280059814453,135.84600830078125 24,0 0,31.99999237060547 205.1280059814453,185.84600830078125 410.2569885253906,31.99999237060547" />
+    </g>
+  </svg>
+);
+
+export default ChevronDown;
