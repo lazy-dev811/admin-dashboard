b578122da60480fcef2a355a241036c3bc64b003
devsun527 <dev.sun0811@gmail.com>
2017-04-10 23:54:46 +0200

    newsfeed: added views logic

--- src/client/app/components/NewsFeed/NewsFeed.jsx
+++ src/client/app/components/NewsFeed/NewsFeed.jsx
@@ -2,13 +2,15 @@ import React, { Component, PropTypes } from 'react';
 
 import Filters from './parts/Filters.jsx';
 import Views from './parts/Views';
-import List from './parts/List.jsx';
+import Sources from './parts/Sources.jsx';
+import Articles from './parts/Articles.jsx';
 
 require('./NewsFeed.scss');
 
 class NewsFeed extends Component {
   componentWillMount() {
     this.props.getSources();
+    this.props.getArticles();
   }
 
   render() {
@@ -16,6 +18,7 @@ class NewsFeed extends Component {
       sources,
       activeSources,
       activeView,
+      activeArticles = [],
       categories,
       activeCategories,
       selectCategory,
@@ -32,13 +35,17 @@ class NewsFeed extends Component {
           News Feed
 
           <div className="newsfeed__options">
-            <Filters
-              categories={categories}
-              activeCategories={activeCategories}
-              sortOptions={sortOptions}
-              selectCategory={selectCategory}
-              selectSort={selectSort}
-            />
+            {
+              activeView === 'articles' &&
+              <Filters
+                categories={categories}
+                activeCategories={activeCategories}
+                sortOptions={sortOptions}
+                selectCategory={selectCategory}
+                selectSort={selectSort}
+              />
+            }
+
             <Views
               views={views}
               activeView={activeView}
@@ -46,7 +53,16 @@ class NewsFeed extends Component {
             />
           </div>
         </div>
-        <List sources={sources} activeSources={activeSources} toggleActiveSource={toggleActiveSource} />
+
+        {
+          activeView === 'sources' &&
+          <Sources sources={sources} activeSources={activeSources} toggleActiveSource={toggleActiveSource} />
+        }
+
+        {
+          activeView === 'articles' &&
+          <Articles activeArticles={activeArticles} />
+        }
       </div>
     );
   }
@@ -59,10 +75,12 @@ NewsFeed.defaultProps = {
 NewsFeed.propTypes = {
   sources: PropTypes.array.isRequired,
   activeSources: PropTypes.array,
-  activeView: PropTypes.bool.isRequired,
+  activeArticles: PropTypes.array.isRequired,
+  activeView: PropTypes.string.isRequired,
   categories: PropTypes.array.isRequired,
   activeCategories: PropTypes.array.isRequired,
   getSources: PropTypes.func.isRequired,
+  getArticles: PropTypes.func.isRequired,
   selectCategory: PropTypes.func.isRequired,
   selectSort: PropTypes.func.isRequired,
   selectView: PropTypes.func.isRequired,
