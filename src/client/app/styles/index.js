a58c24b7b2b6f60bcbc67087df922c4505f7cff5
devsun527 <dev.sun0811@gmail.com>
2017-04-26 20:04:56 +0200

    extend: added styled components

---
+++ src/client/app/styles/index.js
@@ -0,0 +1,11 @@
+// colors
+export const COLOR_BLUE = '#0ad';
+export const COLOR_TEAL = '#12C3E4';
+export const COLOR_TEXT = '#373636';
+export const COLOR_LINK = '#373636';
+export const COLOR_PLACEHOLDER = '#eee';
+export const COLOR_BG = '#006237';
+export const COLOR_BG_ALT = '#373636';
+
+// layout
+export const UNIT = '20px';
