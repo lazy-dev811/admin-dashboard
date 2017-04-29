f4839a0c27b28ee8a8bbc3e95bfb2b321ac6d229
devsun527 <dev.sun0811@gmail.com>
2017-04-29 13:41:12 +0200

    styled-components: added to

--- src/client/app/styles/index.js
+++ src/client/app/styles/index.js
@@ -7,8 +7,14 @@ export const COLOR_PLACEHOLDER = '#eee';
 export const COLOR_BG = '#006237';
 export const COLOR_BG_ALT = '#373636';
 
+export const COLOR_GRAY = '#373636';
+export const COLOR_WHITE = '#fff';
 export const COLOR_GRAY_1 = '#e6e6e6';
+export const COLOR_GRAY_5 = '#999';
 
 // layout
-export const UNIT = '20px';
 export const U = 5;
+export const UNIT_XSM = () => `${U}px`;
+export const UNIT_SM = () => `${U * 2}px`;
+export const UNIT_MD = () => `${U * 3}px`;
+export const UNIT_LG = () => `${U * 4}px`;
