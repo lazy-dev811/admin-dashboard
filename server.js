6fe5f2a703adec9161a2a180bad1093b25c6acb0
devsun527 <dev.sun0811@gmail.com>
2017-05-06 19:41:37 +0200

    misc

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 7777
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 7777;
 
 // using webpack-dev-server and middleware in development environment
-if(process.env.NODE_ENV !== 'production') {
-  var webpackDevMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware');
-  var webpack = require('webpack');
-  var config = require('./webpack.config');
-  var compiler = webpack(config);
+if (process.env.NODE_ENV !== 'production') {
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpack = require('webpack');
+  const config = require('./webpack.config');
+
+  const compiler = webpack(config);
 
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
@@ -17,14 +19,14 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html')
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.listen(PORT, function(error) {
+app.listen(PORT, (error) => {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.info('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
   }
 });
